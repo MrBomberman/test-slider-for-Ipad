16a98a88f1428761d2bf4e73ce102c139c757a47
Kirill <mrbomberman99@gmail.com>
2021-09-29 20:16:54 +0300

    added files for second page

---
+++ src/components/SecondPage/index.js
@@ -0,0 +1,11 @@
+import './secondPage.css'
+
+function secondPage(){
+    return (
+        <div>
+            <p className='title'>ТЕКСТ СООБЩЕНИЯ</p>
+        </div>
+    )
+}
+
+export default secondPage;
